ab3e25a5ca386fb66ebdc6286406edd052785ecb
Abby Mueller <amueller@bowdoin.edu>
2022-09-08 20:56:38 -0400

    finding artist, track, titles given a playlist

--- spotify_part.py
+++ spotify_part.py
@@ -8,32 +8,77 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 #set environment variables https://phoenixnap.com/kb/set-environment-variable-mac
 
-birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+def get_playlist_id():
+    print("What is the id of the playlist?")
+    play_id_num = input()
+    return 'spotify:playlist:' + str(play_id_num)
 
-results = sp.artist_albums(birdy_uri, album_type='album')
-albums = results['items']
-while results['next']:
-    results = sp.next(results)
-    albums.extend(results['items'])
-
-for album in albums:
-    print(album['name'])
-
-
-#playlist iteration from https://github.com/plamere/spotipy/blob/master/examples/playlist_tracks.py 
-pl_id = 'spotify:playlist:1umwX5x9YfdOYZDWR5BjUO'
-offset = 0
-
-while True:
-    response = sp.playlist_items(pl_id,
-                                 offset=offset,
-                                 fields='items.track.id,items.track.name,total',
-                                 additional_types=['track'])
+def get_playlist_track_info(sp, uri):
+    """
+    Get playlist track info and aggregate into dataframes.
+    Args:
+    sp, spotipy credentials object
+    uri, resource id for playlist
     
+    """
+    track_uri = []
+    track_name = []
+    track_artist = []
+
+
+    offset = 0 #tracks how many items in the playlist we look at
+    response = sp.playlist_items(uri,
+                                offset=offset,
+                                fields='items.track.id,items.track.name,items.track.artists, total',
+                                additional_types=['track'])
+
     if len(response['items']) == 0:
-        break
-    
-    print(response['items'])
-    offset = offset + len(response['items'])
-    print(offset, "/", response['total'])
+        print("Invalid playlist- no songs")
+    else:
+        
+        for item in response['items']:
+            #print(item)
+            artists = item["track"]["artists"]
+            track_id = item["track"]["id"]
+            track_n = item["track"]["name"]
+        
+            track_uri.append(track_id)
+            track_name.append(track_n)
+
+            artist_name = artists[0]["name"]
+        
+            if len(artists) > 1:
+                artist2_name = artists[1]["name"]
+                
+
+                track_artist.append([artist_name, artist2_name])
+            else:
+                track_artist.append([artist_name])
+            
+        print(track_uri, track_name, track_artist)
+
+        #print(response['items.track'])
+        offset = offset + len(response['items'])
+        print(offset, "/", response['total'])
+
+
+#1umwX5x9YfdOYZDWR5BjUO
+#playlist iteration from https://github.com/plamere/spotipy/blob/master/examples/playlist_tracks.py 
+#pl_id = 'spotify:playlist:' + str(play_id_num)
+#print(pl_id)
+
+
+#while True:
+
+
+
+def main():
+
+    sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
+    print("x")
+    id = get_playlist_id()
+    get_playlist_track_info(sp, id)
+
+if __name__ == "__main__":
+    main()
